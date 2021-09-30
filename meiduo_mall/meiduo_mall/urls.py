6658dc3567990eac5a9fce665b053e6740e0ec22
Axiejinhao <xjh9836@gmail.com>
2021-09-30 10:52:28 +0800

    子应用

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -1,4 +1,4 @@
-"""meduo_mall URL Configuration
+"""meiduo_mall URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
