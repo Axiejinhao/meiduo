295e14795a80ddc8f8c2919cc0098a695e449ba8
Axiejinhao <xjh9836@gmail.com>
2021-10-20 13:01:36 +0800

    列表实现

--- meiduo_mall/utils/goods.py
+++ meiduo_mall/utils/goods.py
@@ -63,25 +63,25 @@ def get_categories():
 
 
 def get_breadcrumb(category):
-    """接收最低级别的类别, 获取各个类别的名称, 返回"""
+    '''接收最低级别的类别, 获取各个类别的名称, 返回'''
 
-    dicts = {
+    dict = {
         'cat1': '',
         'cat2': '',
         'cat3': '',
     }
 
     if category.parent is None:
-        dicts['cat1'] = category.name
+        dict['cat1'] = category.name
     elif category.parent.parent is None:
-        dicts['cat2'] = category.name
-        dicts['cat1'] = category.parent.name
+        dict['cat2'] = category.name
+        dict['cat1'] = category.parent.name
     else:
-        dicts['cat3'] = category.name
-        dicts['cat2'] = category.parent.name
-        dicts['cat1'] = category.parent.parent.name
+        dict['cat3'] = category.name
+        dict['cat2'] = category.parent.name
+        dict['cat1'] = category.parent.parent.name
 
-    return dicts
+    return dict
 
 
 """
