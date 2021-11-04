e457d8cdd8eee8c96080c7e5302b9aba984ae384
Axiejinhao <xjh9836@gmail.com>
2021-11-04 10:30:32 +0800

    生成支付宝转跳链接

--- meiduo_mall/apps/pay/urls.py
+++ meiduo_mall/apps/pay/urls.py
@@ -1,17 +1,6 @@
-"""
-支付宝为我们测试提供了沙箱(测试)环境
+from django.urls import path
+from apps.pay.views import PayUrlView
 
-
-一.设置公钥和私钥
-
-    美多商城一对(我们弄)
-    genrsa -out app_private_key.pem 2048
-    rsa -in app_private_key.pem -pubout -out app_public_key.pem
-    将app_public_key.pem的内容复制到沙箱应用中
-    支付宝一对(它自己弄)
-
-在整个支付流程中,我们(美多商城)需要做的就是2件事
-1.生成跳转到支付宝的链接
-2.保存交易完成后,支付宝返回的交易流水号
-
-"""
+urlpatterns = [
+    path('payment/<order_id>/', PayUrlView.as_view()),
+]
--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -295,3 +295,11 @@ d: 一星期内的天（0~6，0为星期天）
 CRONJOBS = [
     ('*/1 * * * *', 'apps.contents.crons.generic_meiduo_index', '>> ' + os.path.join(BASE_DIR, 'logs/crontab.log'))
 ]
+
+# 支付宝支付相关
+ALIPAY_APPID = '2021000118642339'
+ALIPAY_DEBUG = True
+ALIPAY_URL = 'https://openapi.alipaydev.com/gateway.do'
+ALIPAY_RETURN_URL = 'http://www.meiduo.site:8080/pay_success.html'
+APP_PRIVATE_KEY_PATH = os.path.join(BASE_DIR, 'apps/pay/key/app_private_key.pem')
+ALIPAY_PUBLIC_KEY_PATH = os.path.join(BASE_DIR, 'apps/pay/key/alipay_public_key.pem')
